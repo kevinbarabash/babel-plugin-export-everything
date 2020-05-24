aeac1435e1c3bca3ffe9081ea1a38da329e9813a
Kevin Barabash <kevinb@khanacademy.org>
2020-05-24 15:52:46 -0400

    set up eslint

--- example/default-variable.js
+++ example/default-variable.js
@@ -1,6 +1,5 @@
 const getString = () => "default";
+// eslint-disable-next-line no-unused-vars
 const useGetString = () => getString();
 
-export {
-    getString as default
-};
+export {getString as default};
