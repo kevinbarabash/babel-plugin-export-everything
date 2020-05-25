00446d58b23ae1801a01bf4564aa940301a09703
Kevin Barabash <kevinb7@gmail.com>
2020-05-25 13:03:41 -0400

    add flow (#9)

--- example/react/page.js
+++ example/react/page.js
@@ -1,27 +1,22 @@
 // @flow
 import * as React from "react";
 
-type HeadingProps = {|
-    children: string,
-|};
-
+type HeadingProps = {children: string};
 class Heading extends React.Component<HeadingProps> {
     render() {
         return <h1>{this.props.children}</h1>;
     }
 }
 
-type BodyProps = {|
-    children: string,
-|};
-
+type BodyProps = {children: string};
 class Body extends React.Component<BodyProps> {
     render() {
         return <p>{this.props.children}</p>;
     }
 }
 
-export default class Page extends React.Component {
+type Props = {};
+export default class Page extends React.Component<Props> {
     render() {
         return (
             <div>
